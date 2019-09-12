efe3b152c50bed93e5ce6c6b3ccab4eb0dd49448
Alex <arakcheyev.alex@gmail.com>
2019-09-12 16:10:17 +0700

    Add titles_by_category_name to Test

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,6 @@
 class Test < ApplicationRecord
+  def self.titles_by_category_name(category_name)
+    categories_ids = Category.where(title: category_name)
+    where(category_id: categories_ids).order(title: :desc).pluck(:title)
+  end
 end
