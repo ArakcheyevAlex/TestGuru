5ae23b7f48f8063d41fd9ac5484e675ffe782e16
Alex <arakcheyev.alex@gmail.com>
2019-09-13 01:13:16 +0700

    Change methods to use joins

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,8 @@
 class Test < ApplicationRecord
   def self.titles_by_category_name(category_name)
-    categories_ids = Category.where(title: category_name)
-    where(category_id: categories_ids).order(title: :desc).pluck(:title)
+    joins('JOIN categories ON categories.id = category_id')
+      .where(categories: { title: category_name })
+      .order(title: :desc)
+      .pluck(:title)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   def tests_by_level(level)
-    users_tests_ids = TestResult.where(user_id: id).select(:test_id)
-
-    Test.where(id: users_tests_ids, level: level)
+    Test.joins('INNER JOIN test_results ON test_results.test_id = tests.id')
+        .where(test_results: { user_id: id })
+        .where(tests: { level: level })
   end
 end
