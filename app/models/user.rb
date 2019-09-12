d3abac9714f9fc5988ca24f4f41dbf387d9b1d5b
Alex <arakcheyev.alex@gmail.com>
2019-09-12 15:57:32 +0700

    Add tests_by_level to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  def tests_by_level(level)
+    users_tests_ids = TestResult.where(user_id: id).select(:test_id)
+
+    Test.where(id: users_tests_ids, level: level)
+  end
 end
