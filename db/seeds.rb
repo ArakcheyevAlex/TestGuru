518922e994e3ae37764ef0985b15b7fe5607f01b
Alex <arakcheyev.alex@gmail.com>
2019-09-17 13:24:56 +0500

    Refactor seeds.rb with using associations

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,60 +10,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' },
 #   { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-categories = Category.create!(
-  [
-    { title: 'Programming' },
-    { title: 'Math' },
-    { title: 'Chemistry' }
-  ]
-)
-
-tests = Test.create!(
-  [
-    { title: 'Ruby Beginners', category_id: categories.first.id, level: 1 },
-    { title: 'Go Professionals', category_id: categories.first.id, level: 3 },
-    { title: 'Crystal First Steps', category_id: categories.first.id, level: 2 },
-    { title: 'Algebra', category_id: categories.second.id, level: 1 },
-    { title: 'Differential equation', category_id: categories.second.id, level: 2 },
-    { title: 'Discontinuous Galerkin method', category_id: categories.second.id, level: 3 },
-    { title: 'Chemical physics', category_id: categories.third.id, level: 1 },
-    { title: 'Organometallic chemistry', category_id: categories.third.id, level: 2 },
-    { title: 'Biochemistry', category_id: categories.third.id, level: 3 }
-  ]
-)
-
-questions = Question.create!(
-  [
-    { body: 'RB test Question 1', test_id: tests.first.id },
-    { body: 'RB test Question 2', test_id: tests.first.id },
-    { body: 'RB test Question 3', test_id: tests.first.id },
-    { body: 'Go test Question 1 body', test_id: tests.second.id },
-    { body: 'Go test Question 2 body', test_id: tests.second.id },
-    { body: 'Go test Question 3 body', test_id: tests.second.id },
-    { body: 'Crystal test Question 1 body', test_id: tests.third.id },
-    { body: 'Crystal test Question 2 body', test_id: tests.third.id },
-    { body: 'Crystal test Question 3 body', test_id: tests.third.id }
-  ]
-)
-
-Answer.create!(
-  [
-    { body: 'answer A', question_id: questions.first.id, correct: true },
-    { body: 'answer B', question_id: questions.first.id },
-    { body: 'answer C', question_id: questions.first.id },
-    { body: 'answer A', question_id: questions.second.id },
-    { body: 'answer B', question_id: questions.second.id, correct: true },
-    { body: 'answer C', question_id: questions.second.id },
-    { body: 'answer A', question_id: questions.third.id },
-    { body: 'answer B', question_id: questions.third.id },
-    { body: 'answer C', question_id: questions.third.id, correct: true },
-    { body: 'answer A', question_id: questions.fourth.id, correct: true },
-    { body: 'answer B', question_id: questions.fourth.id },
-    { body: 'answer C', question_id: questions.fourth.id }
-  ]
-)
-
-users = User.create!(
+User.create!(
   [
     { name: 'Bob' },
     { name: 'Jack' },
@@ -74,14 +21,66 @@ users = User.create!(
   ]
 )
 
+Category.create!(
+  [
+    { title: 'Programming' },
+    { title: 'Math' },
+    { title: 'Chemistry' }
+  ]
+)
+
+Test.create!(
+  [
+    { title: 'Ruby Beginners', category: Category.first, level: 1, author: User.first },
+    { title: 'Go Professionals', category: Category.first, level: 3, author: User.first },
+    { title: 'Crystal First Steps', category: Category.first, level: 2, author: User.first },
+    { title: 'Algebra', category: Category.second, level: 1, author: User.second },
+    { title: 'Differential equation', category: Category.second, level: 2, author: User.second },
+    { title: 'Discontinuous Galerkin method', category: Category.second, level: 3, author: User.second },
+    { title: 'Chemical physics', category: Category.third, level: 1, author: User.third },
+    { title: 'Organometallic chemistry', category: Category.third, level: 2, author: User.third },
+    { title: 'Biochemistry', category: Category.third, level: 3, author: User.third }
+  ]
+)
+
+Question.create!(
+  [
+    { body: 'RB test Question 1', test: Test.first },
+    { body: 'RB test Question 2', test: Test.first },
+    { body: 'RB test Question 3', test: Test.first },
+    { body: 'Go test Question 1 body', test: Test.second },
+    { body: 'Go test Question 2 body', test: Test.second },
+    { body: 'Go test Question 3 body', test: Test.second },
+    { body: 'Crystal test Question 1 body', test: Test.third },
+    { body: 'Crystal test Question 2 body', test: Test.third },
+    { body: 'Crystal test Question 3 body', test: Test.third }
+  ]
+)
+
+Answer.create!(
+  [
+    { body: 'answer A', question: Question.first, correct: true },
+    { body: 'answer B', question: Question.first },
+    { body: 'answer C', question: Question.first },
+    { body: 'answer A', question: Question.second },
+    { body: 'answer B', question: Question.second, correct: true },
+    { body: 'answer C', question: Question.second },
+    { body: 'answer A', question: Question.third },
+    { body: 'answer B', question: Question.third },
+    { body: 'answer C', question: Question.third, correct: true },
+    { body: 'answer A', question: Question.fourth, correct: true },
+    { body: 'answer B', question: Question.fourth },
+    { body: 'answer C', question: Question.fourth }
+  ]
+)
+
 TestResult.create!(
   [
-    { user_id: users.first.id, test_id: tests.first.id, result: 10 },
-    { user_id: users.first.id, test_id: tests.second.id, result: 9 },
-    { user_id: users.first.id, test_id: tests.third.id, result: 12 },
-    { user_id: users.second.id, test_id: tests.first.id, result: 2 },
-    { user_id: users.second.id, test_id: tests.second.id, result: 5 },
-    { user_id: users.second.id, test_id: tests.third.id, result: 9 }
+    { user: User.first, test: Test.first, result: 10 },
+    { user: User.first, test: Test.second, result: 9 },
+    { user: User.first, test: Test.third, result: 12 },
+    { user: User.second, test: Test.first, result: 2 },
+    { user: User.second, test: Test.second, result: 5 },
+    { user: User.second, test: Test.third, result: 9 }
   ]
-
 )
