24a7cd52e80819cf91e9b2a30f19d36d7b542a04
Alex <arakcheyev.alex@gmail.com>
2019-09-17 13:33:42 +0500

    Refactor User#tests_by_level using assotiations

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,6 @@ class User < ApplicationRecord
            dependent: :nullify
 
   def tests_by_level(level)
-    Test.joins('INNER JOIN test_results ON test_results.test_id = tests.id')
-        .where(test_results: { user_id: id })
-        .where(tests: { level: level })
+    tests.where(level: level)
   end
 end
