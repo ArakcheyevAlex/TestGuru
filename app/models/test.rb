427e825cdd10e13ae8f0bff6c6d09ad903707475
Alex <arakcheyev.alex@gmail.com>
2019-09-17 13:02:20 +0500

    Associations fix

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,5 +1,9 @@
 class Test < ApplicationRecord
   belongs_to :category
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  has_many :questions, dependent: :destroy
+  has_many :test_results, dependent: :destroy
+  has_many :users, through: :test_results
 
   def self.titles_by_category_name(category_name)
     joins('JOIN categories ON categories.id = category_id')
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ApplicationRecord
-  has_many :tests, through: :test_result
+  has_many :test_results
+  has_many :tests, through: :test_results
+  has_many :author_tests,
+           class_name: 'Test',
+           foreign_key: 'author_id',
+           dependent: :nullify
 
   def tests_by_level(level)
     Test.joins('INNER JOIN test_results ON test_results.test_id = tests.id')
