48dabc00db50773374296fddb63c13368b97a7aa
Alex Arakcheyev <arakcheyev.alex@gmail.com>
2019-09-25 16:33:13 +0500

    Add view for Question

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,4 +3,8 @@ class QuestionsController < ApplicationController
     @test = Test.find(params[:test_id])
     @questions = @test.questions
   end
+
+  def show
+    @question = Question.find(params[:id])
+  end
 end
