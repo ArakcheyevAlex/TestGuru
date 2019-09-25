a5571786f85aa609844d352a86ba8017dd4d333d
Alex Arakcheyev <arakcheyev.alex@gmail.com>
2019-09-25 16:50:32 +0500

    Add new Questions creation

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -7,4 +7,26 @@ class QuestionsController < ApplicationController
   def show
     @question = Question.find(params[:id])
   end
+
+  def new
+    @test = Test.find(params[:test_id])
+    @question = @test.questions.new
+  end
+
+  def create
+    @test = Test.find(params[:test_id])
+    @question = @test.questions.new(question_params)
+
+    if @question.save
+      redirect_to test_questions_path(@test)
+    else
+      render plain: 'Error with saving question'
+    end
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:test_id, :body)
+  end
 end
