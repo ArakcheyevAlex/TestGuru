f9f7e4eea967eb30eac7e9c8f653e1fb6972f384
Alex Arakcheyev <arakcheyev.alex@gmail.com>
2019-10-14 11:58:25 +0500

    Add controller and basic views for Tests

---
+++ app/controllers/tests_controller.rb
@@ -0,0 +1,28 @@
+class TestsController < ApplicationController
+  def index
+    @tests = Test.all
+  end
+
+  def new
+    @test = Test.new
+  end
+
+  def create
+    @test = Test.new(test_params)
+    if @test.save
+      redirect_to @test
+    else
+      render :new
+    end
+  end
+
+  def show
+    @test = Test.find(params[:id])
+  end
+
+  private
+
+  def test_params
+    params.require(:test).permit(:title, :level, :category_id, :author_id)
+  end
+end
