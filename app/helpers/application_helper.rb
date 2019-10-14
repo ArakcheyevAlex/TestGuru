51fa12d68bf3f63681a54d5c15d2f339af61a8f0
Alex Arakcheyev <arakcheyev.alex@gmail.com>
2019-10-14 20:52:09 +0500

    Add helper for generate link to GitHub repo URL

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,8 @@ module ApplicationHelper
   def current_year
     Time.zone.now.year
   end
+
+  def github_url(author, repo)
+    link_to repo, "https://github.com/#{author}/#{repo}", target: '_blank'
+  end
 end
