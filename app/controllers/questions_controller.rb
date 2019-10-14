f9f612cb5a02e353035e8e6b15b036adb0c2adf6
Alex Arakcheyev <arakcheyev.alex@gmail.com>
2019-10-14 20:19:47 +0500

    Add Edit action to Question

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,6 @@
 class QuestionsController < ApplicationController
   before_action :set_test, only: %i[index new create]
-  before_action :set_question, only: %i[show destroy]
+  before_action :set_question, only: %i[show edit update destroy]
 
   rescue_from ActiveRecord::RecordNotFound,
               with: :rescue_with_question_not_found
@@ -22,6 +22,16 @@ class QuestionsController < ApplicationController
       redirect_to test_questions_path(@test)
     else
       render plain: 'Error with saving question'
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @question.update(question_params)
+      redirect_to @question
+    else
+      render :edit
     end
   end
 
