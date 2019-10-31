f627f965e725bbb0e7426f4bcee5ba6e6409db10
Alex Arakcheyev <arakcheyev.alex@gmail.com>
2019-10-31 08:40:39 +0500

    Set next question before validation

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -3,11 +3,19 @@ class TestPassage < ApplicationRecord
   belongs_to :test
   belongs_to :current_question, class_name: 'Question', optional: true
 
-  before_validation :before_validation_set_first_question, on: :create
+  before_validation :before_validation_set_current_question
 
   private
 
-  def before_validation_set_first_question
-    self.current_question = test.questions.first if test.present?
+  def next_question
+    if current_question
+      test.questions.where('id > ?', current_question.id).first
+    else
+      test.questions.first
+    end
+  end
+
+  def before_validation_set_current_question
+    self.current_question = next_question
   end
 end
