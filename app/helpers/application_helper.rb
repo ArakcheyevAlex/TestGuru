6cc36aed79354cdbd622d76ae23aa03005b6131d
Alex Arakcheyev <arakcheyev.alex@gmail.com>
2020-01-24 11:13:54 +0700

    Add partial for flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,8 @@ module ApplicationHelper
   def github_url(author, repo)
     link_to repo, "https://github.com/#{author}/#{repo}", target: '_blank'
   end
+
+  def flash_message(message, type)
+    content_tag :div, message, class: "flash #{type}"
+  end
 end
